519e8e1d22bf6906ef94525d93e94a6d61a748b7
SemeAIPletinnya <adelayida0403@gmail.com>
2025-12-07 14:34:52 +0200

    por_core/__init__.py

--- por_core/__init__.py
+++ por_core/__init__.py
@@ -1,11 +1,13 @@
-"""
-Proof-of-Resonance Core Package
-Initializes the PoR engine modules.
-"""
+# por_core/__init__.py
+from .config import PoRConfig
+from .metrics import stability_score, coherence
+from .phase_lock import phase_lock
+from .simulator import ResonanceSimulator
 
 __all__ = [
-    "config",
-    "metrics",
+    "PoRConfig",
+    "stability_score",
+    "coherence",
     "phase_lock",
-    "simulator",
+    "ResonanceSimulator",
 ]
