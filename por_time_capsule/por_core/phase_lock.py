d736ba98827755d2a69cf93e7e941a67bab5530f
Anton Sememenko <adelayida0403@gmail.com>
2025-12-10 21:00:34 +0200

    Update phase_lock.py

--- por_time_capsule/por_core/phase_lock.py
+++ por_time_capsule/por_core/phase_lock.py
@@ -1,1 +1,8 @@
+import numpy as np
+
+def phase_lock_value(chain):
+    """Phase-lock index (0..1) similar to Kuramoto order parameter."""
+    phases = np.exp(1j * np.array(chain))
+    R = np.abs(np.mean(phases))
+    return float(R)  # 1 = perfect coherence
 
